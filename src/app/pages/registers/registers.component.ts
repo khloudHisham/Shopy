702e5d1d70cef9d94998b74a69264e10ddfd6055
MSB <khokhakammar@gmail.com>
2025-03-01 14:51:55 +0200

    main components created

---
+++ src/app/pages/registers/registers.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registers',
+  imports: [],
+  templateUrl: './registers.component.html',
+  styleUrl: './registers.component.scss'
+})
+export class RegistersComponent {
+
+}
